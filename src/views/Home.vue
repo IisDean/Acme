9b766d94d5e1b206b89521b6f04d7d4af8bc52d4
IisDean <781775946@qq.com>
2019-09-29 17:08:10 +0800

    新加入首屏动画效果

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <HomeBg msg="你好！这里是IisDean的个人网站，如有意深入交流，可加wx：IisDean"/>
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HomeBg from '@/components/HomeBg.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HomeBg
+  }
+}
+</script>
